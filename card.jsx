6d6238459a349b42fd1e2cd83885db0de83984e4
nazariy2025 <n.romanenchuk@gmail.com>
2025-04-19 19:14:45 +0300

    Delete card.jsx

--- card.jsx
+++
@@ -1,2 +0,0 @@
-export function Card({ children, className }) { return <div className={`rounded-xl shadow-md ${className}`}>{children}</div>; }
-export function CardContent({ children, className }) { return <div className={className}>{children}</div>; }
