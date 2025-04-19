2a07f1c098c6052d0a850677e2dcfb6681add532
nazariy2025 <n.romanenchuk@gmail.com>
2025-04-19 19:03:22 +0300

    Add files via upload

---
+++ card.jsx
@@ -0,0 +1,2 @@
+export function Card({ children, className }) { return <div className={`rounded-xl shadow-md ${className}`}>{children}</div>; }
+export function CardContent({ children, className }) { return <div className={className}>{children}</div>; }
